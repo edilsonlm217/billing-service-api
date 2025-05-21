af4eb3f28c2edaa7ba2a106509cf30931efbe38b
Seu Nome <edilsonlm217@hotmail.com>
2025-05-21 18:05:31 -0400

    refactor: replaced migration by a new one with no user_id prop

--- lib/db/migrations/0001_skinny_wildside.sql
+++ lib/db/migrations/0001_skinny_wildside.sql
@@ -1,12 +1,10 @@
 CREATE TABLE "api_keys" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"key" varchar(255) NOT NULL,
-	"user_id" integer NOT NULL,
 	"team_id" integer NOT NULL,
 	"active" boolean DEFAULT true NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "api_keys_key_unique" UNIQUE("key")
 );
 --> statement-breakpoint
-ALTER TABLE "api_keys" ADD CONSTRAINT "api_keys_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "api_keys" ADD CONSTRAINT "api_keys_team_id_teams_id_fk" FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON DELETE no action ON UPDATE no action;
