e47c9e74e984fbd9cd61148eb89b3e3d2d33259b
Seu Nome <edilsonlm217@hotmail.com>
2025-05-22 17:19:26 -0400

    debug: turned subscription validation off

--- app/api/session/[sessionId]/send-message/route.ts
+++ app/api/session/[sessionId]/send-message/route.ts
@@ -2,42 +2,43 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getApiKeyWithTeam } from '@/lib/db/queries';
 
 export async function POST(req: NextRequest) {
-  const authHeader = req.headers.get('authorization');
+  console.log(req);
+  // const authHeader = req.headers.get('authorization');
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-  }
-
-  const apiKey = authHeader.split(' ')[1];
-  const result = await getApiKeyWithTeam(apiKey);
-
-  if (!result || !result.apiKey.active) {
-    return NextResponse.json({ error: 'Invalid or inactive API key' }, { status: 403 });
-  }
-
-  const { team } = result;
-
-  // const status = team.subscriptionStatus;
-  // if (status !== 'active' && status !== 'trialing') {
-  //   return NextResponse.json(
-  //     { error: 'Subscription inactive or expired' },
-  //     { status: 402 }
-  //   );
+  // if (!authHeader || !authHeader.startsWith('Bearer ')) {
+  //   return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   // }
 
-  const body = await req.json();
-  const { to, text } = body;
-  const sessionId = 'cliente-whatsapp-01';
+  // const apiKey = authHeader.split(' ')[1];
+  // const result = await getApiKeyWithTeam(apiKey);
 
-  const baileysRes = await fetch(`${process.env.BAILEYS_API_URL}/sessions/${sessionId}/send-message`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ to, message: text }),
-  });
+  // if (!result || !result.apiKey.active) {
+  //   return NextResponse.json({ error: 'Invalid or inactive API key' }, { status: 403 });
+  // }
 
-  const baileysJson = await baileysRes.json();
+  // const { team } = result;
 
-  return NextResponse.json(baileysJson, { status: baileysRes.status });
+  // // const status = team.subscriptionStatus;
+  // // if (status !== 'active' && status !== 'trialing') {
+  // //   return NextResponse.json(
+  // //     { error: 'Subscription inactive or expired' },
+  // //     { status: 402 }
+  // //   );
+  // // }
+
+  // const body = await req.json();
+  // const { to, text } = body;
+  // const sessionId = 'cliente-whatsapp-01';
+
+  // const baileysRes = await fetch(`${process.env.BAILEYS_API_URL}/sessions/${sessionId}/send-message`, {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({ to, message: text }),
+  // });
+
+  // const baileysJson = await baileysRes.json();
+
+  // return NextResponse.json(baileysJson, { status: baileysRes.status });
 }
