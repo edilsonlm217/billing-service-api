7b09a610534867bed09329e6fd421ab4b36e0706
Edilson R Lima <edilsonlm217@hotmail.com>
2025-07-05 16:11:44 -0400

    fix(navigation): update dashboard link to point to subscription management page

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Button } from '@/components/ui/button';
-import { Users, Settings, Shield, Activity, Menu, Send, MessageCircle } from 'lucide-react';
+import { Users, Settings, Shield, Activity, Menu, Send, MessageCircle, HouseIcon } from 'lucide-react';
 
 export default function DashboardLayout({
   children
@@ -15,10 +15,10 @@ export default function DashboardLayout({
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const navItems = [
-    { href: '/dashboard', icon: Users, label: 'Team' },
+    { href: '/dashboard/subscription', icon: Users, label: 'Assinatura' },
     { href: '/dashboard/general', icon: Settings, label: 'General' },
-    { href: '/dashboard/activity', icon: Activity, label: 'Activity' },
-    { href: '/dashboard/security', icon: Shield, label: 'Security' },
+    // { href: '/dashboard/activity', icon: Activity, label: 'Activity' },
+    // { href: '/dashboard/security', icon: Shield, label: 'Security' },
     { href: '/dashboard/session', icon: Send, label: 'Gerenciar Sessão' },
     { href: '/dashboard/messages', icon: MessageCircle, label: 'Messages' },
     { href: '/dashboard/api-keys', icon: MessageCircle, label: 'Chaves de API' },
